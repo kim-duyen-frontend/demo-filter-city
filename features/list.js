10b7c8f135597d9761882710cab4943e1b05bf17
kim-duyen-frontend <ntduyen255@gmail.com>
2022-09-30 21:16:27 +0700

    adjust file

--- features/list.js
+++ features/list.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import data from "../data.json";
 import { v4 as uuidv4 } from 'uuid';
+import styles from "../styles/list.module.scss";
 
 const List = () => {
     const [dataHouse] = useState(data);
 
     return (
-        <div className="list">
+        <div className={styles.list}>
             {dataHouse.map((item) => (
                 <div key={uuidv4()}>
                     <p>{item.title}</p>
